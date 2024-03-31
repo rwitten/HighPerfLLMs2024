0916b67dc584e2a184b5ad088d6f14c7080549ab
Rafi Witten <rwitten@google.com>
2024-03-31 02:21:13 +0000

    s05 followup

--- s05/during_class_attention.py
+++ s05/during_class_attention.py
@@ -2,10 +2,26 @@ import jax
 from jax.experimental.pallas.ops import attention as pallas_attention
 
 BATCH = 1
-HEADS = 4
+HEADS = 1
 SEQUENCE = 2048
 HEAD_DIM = 128
 
 Q = jax.random.normal( jax.random.key(0), (BATCH, SEQUENCE, HEADS, HEAD_DIM))
 K = jax.random.normal( jax.random.key(1), (BATCH, SEQUENCE, HEADS, HEAD_DIM))
 V = jax.random.normal( jax.random.key(2), (BATCH, SEQUENCE, HEADS, HEAD_DIM))
+
+
+
+def attention_ourselves(_Q, _K, _V):
+    _weights_unnormalized = jax.numpy.einsum("BSHD,BTHD->BHST", _Q, _K)
+    _weights_unnormalized_to_zero_out = jax.numpy.triu( jax.numpy.ones((SEQUENCE,SEQUENCE), jax.numpy.bfloat16), 1)
+    _weights = jax.nn.softmax(_weights_unnormalized - 1e6 * _weights_unnormalized_to_zero_out)  ### Creating something of size (B,HEADS, SEQUENCE, SEQUENCE)
+    #print(f"{_weights.size=}")
+    output = jax.numpy.einsum("BHST,BTHD->BSHD", _weights, _V)
+
+    return output
+
+attn_ourselves_value = attention_ourselves(Q,K,V)
+attn_value = pallas_attention.mha_reference(Q, K, V,  segment_ids=None, causal=True)
+
+assert jax.numpy.allclose(attn_ourselves_value, attn_value, atol=1e-1, rtol=1e-1)
